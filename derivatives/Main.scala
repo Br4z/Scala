648bfbbb72c79717803a96bcf95c0928a35481ba
Br4z <67658575+Br4z@users.noreply.github.com>
2025-06-01 15:58:28 -0500

    finish derivatives workshop

---
+++ derivatives/Main.scala
@@ -0,0 +1,40 @@
+import derivatives.*
+
+
+object Main extends App {
+	val f: Double => Double = x => x * x // f(x) = x^2
+	val g: Double => Double = x => Math.sin(x) // g(x) = sin(x)
+
+	// Test points
+	val x = Math.PI / 4 // π / 4 ≈ 0.785
+	var result = 0.0
+
+	// 1.1: Basic derivative
+	println("1.1 Basic derivatives:")
+	println(s"f'(${x}) = ${derivative(f)(x)}") // 2x = 1.57
+	println(s"g'(${x}) = ${derivative(g)(x)}") // cos(x) = 0.707
+	println()
+
+	// 1.2.1: Addition of derivatives
+	println("1.2.1 Addition of derivatives:")
+	result = derivative_addition(f, g)(x)
+	println(s"(f + g)'(${x}) = ${result}") // 2.27790...
+	println()
+
+	// 1.2.2: Subtraction of derivatives
+	println("1.2.2 Subtraction of derivatives:")
+	result = derivative_subtraction(f, g)(x)
+	println(s"(f - g)'(${x}) = ${result}") // 0.86368...
+	println()
+
+	// 1.2.3: Product rule
+	println("1.2.3 Product rule:")
+	result = derivative_multiplication(f, g)(x)
+	println(s"(f * g)'(${x}) = ${result}") // 1.54689...
+	println()
+
+	// 1.2.4: Quotient rule
+	println("1.2.4 Quotient rule:")
+	result = derivative_division(f, g)(x)
+	println(s"(f / g)'(${x}) = ${result}") // 1.34908...
+}
--- derivatives/package.scala
+++ derivatives/package.scala
@@ -1,13 +1,88 @@
-package derivates {
+package object derivatives {
+	/* -------------------------------------------------------------------------- */
+	/*                                     1.1                                    */
+	/* -------------------------------------------------------------------------- */
 
-
-	def derivada(f: Double => Double): Double => Double = {
-		def f_prime(x_0: Double): Double = {
+	/**
+	 * Creates a generic numerical derivative function using the five-point stencil
+	 * approximation. Given any real-to-real function "f", this returns a new function
+	 * that approximates f′(x) at each point x.
+	 *
+	 * @param f The original function to differentiate (Double => Double).
+	 * @return a new function (Double => Double) that, when given x, returns the approximate derivative f′(x).
+	 */
+	def derivative(f: Double => Double): Double => Double = {
+		(x: Double) => {
 			val h = 0.1
-
-			(f(x_0 - 2 * h) - 8 * f(x_0 - h) + 8 * f(x_0 + h) - f(x_0 + 2 * h)) / (12 * h)
+			(f(x - 2 * h) - 8 * f(x - h) + 8 * f(x + h) - f(x + 2 * h)) / (12 * h)
 		}
+	}
 
-		f_prime
+	/* -------------------------------------------------------------------------- */
+	/*                                     1.2                                    */
+	/* -------------------------------------------------------------------------- */
+
+	/* ---------------------------------- 1.2.1 --------------------------------- */
+
+	/**
+	 * Constructs the derivative of the sum of two functions. Given functions "f" and "g",
+	 * returns a new function that computes (f + g)′(x) = f′(x) + g′(x).
+	 *
+	 * @param f First function of type (Double => Double).
+	 * @param g Second function of type (Double => Double).
+	 * @return a function (Double => Double) that, for each x, returns (f + g)′(x).
+	 */
+	def derivative_addition(f: Double => Double, g: Double => Double): Double => Double = {
+		(x: Double) => {
+			derivative(f)(x) + derivative(g)(x)
+		}
+	}
+
+	/* ---------------------------------- 1.2.2 --------------------------------- */
+
+	/**
+	 * Constructs the derivative of the difference of two functions. Given functions "f" and "g",
+	 * returns a new function that computes (f − g)′(x) = f′(x) − g′(x).
+	 *
+	 * @param f First function of type (Double => Double).
+	 * @param g Second function of type (Double => Double).
+	 * @return a function (Double => Double) that, for each x, returns (f − g)′(x).
+	 */
+	def derivative_subtraction(f: Double => Double, g: Double => Double): Double => Double = {
+		(x: Double) => {
+			derivative(f)(x) - derivative(g)(x)
+		}
+	}
+
+	/* ---------------------------------- 1.2.3 --------------------------------- */
+
+	/**
+	 * Constructs the derivative of the product of two functions. Given functions "f" and "g",
+	 * returns a new function that computes (f * g)′(x) = f′(x) * g(x) + f(x) * g′(x).
+	 *
+	 * @param f function of type (Double => Double).
+	 * @param g Second function of type (Double => Double).
+	 * @return a function (Double => Double) that, for each x, returns (f * g)′(x).
+	 */
+	def derivative_multiplication(f: Double => Double, g: Double => Double): Double => Double = {
+		(x: Double) => {
+			derivative(f)(x) * g(x) + f(x) * derivative(g)(x)
+		}
+	}
+
+	/* ---------------------------------- 1.2.4 --------------------------------- */
+
+	/**
+	 * Constructs the derivative of the quotient of two functions. Given functions "f" and "g",
+	 * returns a new function that computes (f / g)′(x) = [f′(x)·g(x) − f(x)·g′(x)] / [g(x)²].
+	 *
+	 * @param f Numerator function of type (Double => Double).
+	 * @param g Denominator function of type (Double => Double). Must not be zero at x.
+	 * @return A function (Double => Double) that, for each x, returns (f / g)′(x).
+	 */
+	def derivative_division(f: Double => Double, g: Double => Double): Double => Double = {
+		(x: Double) => {
+			(derivative(f)(x) * g(x) - f(x) * derivative(g)(x)) / (g(x) * g(x))
+		}
 	}
 }
